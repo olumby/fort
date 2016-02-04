cb2a3efa3cab8fbcd31e6d8636e2ac43d2d0392e
Oliver Lumby <me@oliverlumby.com>
2016-02-04 14:34:34 +0100

    set up webserver and sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -2,23 +2,33 @@ var gulp = require('gulp');
 var cssnano = require('gulp-cssnano');
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
-//var watch = require('gulp-watch');
+var webserver = require('gulp-webserver');
+var sourcemaps = require('gulp-sourcemaps');
 var rename = require('gulp-rename');
 
 gulp.task('build:css', function () {
     return gulp.src('fort/fort.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass())
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist'));
 });
 
 gulp.task('build:min', function() {
     return gulp.src('fort/fort.scss')
+        .pipe(sourcemaps.init())
         .pipe(sass({outputStyle: 'compressed'}))
         .pipe(rename('fort.min.css'))
+        .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist'));
 })
 
 
-gulp.task('watch', function () {
+gulp.task('run', function () {
+    gulp.src('.')
+        .pipe(webserver({
+            livereload: true,
+            open: true
+        }));
     gulp.watch('fort/**/*.scss', ['build:css', 'build:min']);
 });
